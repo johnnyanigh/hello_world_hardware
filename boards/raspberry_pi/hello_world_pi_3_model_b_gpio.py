4cdca4f9cf15424a9a4e26300d18083d09432e34
Johnny N <johnny.a.nigh@gmail.com>
2019-10-24 21:19:24 -0700

    update: Edit comments for Pi 3 Python code

--- boards/raspberry_pi/hello_world_pi_3_model_b_gpio.py
+++ boards/raspberry_pi/hello_world_pi_3_model_b_gpio.py
@@ -1,16 +1,16 @@
-#import the GPIO and time package
+# Import the GPIO and other packages
 import RPi.GPIO as GPIO
 import time
 
-# Print message in the console
-print("Hello, world from Raspberry Pi 3 Model B!")
+# Print a message in the console
+print("Hello, world from Raspberry Pi 3!")
 
 GPIO.setmode(GPIO.BOARD)
 
-# Drive GPIO4 (pin 7)
+# Drive GPIO4/BCM 4 (pin 7)
 GPIO.setup(7, GPIO.OUT)
 
-# loop through 50 times, on/off for 1 second
+# Loop through 50 times to toggle the pin on/off for 1 second each
 for i in range(50):
     GPIO.output(7,True)
     time.sleep(1)
